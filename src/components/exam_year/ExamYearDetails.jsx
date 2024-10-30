4d10a28bf28e1ca415eb7a27ca3b3d2028670ff3
RubayedMunna <jucse29.370@gmail.com>
2024-10-31 00:52:34 +0600

    Working on Assign Course teacher

--- src/components/exam_year/ExamYearDetails.jsx
+++ src/components/exam_year/ExamYearDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
-import { Card, Container, ListGroup, Row, Col, Button } from 'react-bootstrap';
+import { Card, Container, ListGroup, Row, Col, Button, Form } from 'react-bootstrap';
 
 import '../../App.css';
 
@@ -11,7 +11,11 @@ const ExamYearDetails = () => {
     const [examYearDetails, setExamYearDetails] = useState(null);
     const [courses, setCourses] = useState([]);
     const [classRepresentative, setClassRepresentative] = useState(null);
-    const [studentProfile, setStudentProfile] = useState(null); // New state for student profile
+    const [studentProfile, setStudentProfile] = useState(null);
+    const [teachers, setTeachers] = useState([]);
+    const [assignedCourseTeacher, setAssignedCourseTeacher] = useState([]);
+    const [selectedDepartment, setSelectedDepartment] = useState('');
+    const [selectedTeacher, setSelectedTeacher] = useState({});
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -51,6 +55,25 @@ const ExamYearDetails = () => {
                         headers: { Authorization: `Bearer ${token}` }
                     });
                     setCourses(coursesResponse.data);
+
+                    const coursesData = coursesResponse.data;
+                    console.log('course data:', coursesResponse.data);
+
+                    coursesData.forEach(async (course) => {
+                        console.log('Sending course_id:', course.course_id, 'and teacher_id: null');
+                        try {
+                            await axios.post('http://localhost:5002/api/assign-course', {
+                                course_id: course.course_id,
+                                exam_year_id: exam_year_id
+                            }, {
+                                headers: { Authorization: `Bearer ${token}` }
+                            });
+                        } catch (error) {
+                            console.error('Error assigning course teacher:', error);
+                        }
+                    });
+
+
                 } catch (error) {
                     console.error('Error fetching data:', error);
                     if (error.response && error.response.status === 401) {
@@ -64,20 +87,14 @@ const ExamYearDetails = () => {
                     const crResponse = await axios.get(`http://localhost:5002/api/class-representative/${exam_year_id}`, {
                         headers: { Authorization: `Bearer ${token}` }
                     });
-
-                    // Assuming crResponse.data is an array and you want the first item
                     const classRepData = crResponse.data[0];
                     setClassRepresentative(classRepData);
 
-                    console.log(classRepData.student_id);
-
-                    // Fetch student profile if student_id is present
                     if (classRepData?.student_id) {
                         const studentProfileResponse = await axios.get(`http://localhost:5002/api/student-profile/${classRepData.student_id}`, {
                             headers: { Authorization: `Bearer ${token}` }
                         });
                         setStudentProfile(studentProfileResponse.data);
-                        console.log(studentProfileResponse.data);
                     }
                 } catch (error) {
                     console.error('Error fetching data:', error);
@@ -87,15 +104,80 @@ const ExamYearDetails = () => {
                 }
             };
 
+            const fetchAllTeachers = async () => {
+                try {
+                    const teacherResponse = await axios.get('http://localhost:5002/api/all-teachers/', {
+                        headers: { Authorization: `Bearer ${token}` }
+                    });
+                    setTeachers(teacherResponse.data);
+                    console.log(teacherResponse.data);
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                    if (error.response && error.response.status === 401) {
+                        navigate('/login');
+                    }
+                }
+            };
+
+            const fetchAssignedCourseTeacher = async () => {
+                try {
+                    const assignedCourseTeacherResponse = await axios.get(`http://localhost:5002/api/assigned-course-teachers/${exam_year_id}`, {
+                        headers: { Authorization: `Bearer ${token}` }
+                    });
+                    setAssignedCourseTeacher(assignedCourseTeacherResponse.data);
+                    console.log(assignedCourseTeacher);
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                    if (error.response && error.response.status === 401) {
+                        navigate('/login');
+                    }
+                }
+            };
 
             fetchExamDetails();
             fetchExamCommittee();
             fetchCourses();
             fetchClassRepresentative();
+            fetchAllTeachers();
+            fetchAssignedCourseTeacher();
         } else {
             navigate('/login');
         }
     }, [exam_year_id, navigate]);
+
+    const handleDepartmentChange = (e) => {
+        setSelectedDepartment(e.target.value);
+        setSelectedTeacher({});
+    };
+
+    const uniqueDepartments = [...new Set(teachers.map((teacher) => teacher.dept_id))];
+
+    const filteredTeachers = teachers.filter(teacher => teacher.dept_id === selectedDepartment);
+
+    const handleTeacherChange = (courseId, teacherId) => {
+        setSelectedTeacher(prevState => ({ ...prevState, [courseId]: teacherId }));
+    };
+
+    const handleUpdateCourseTeacher = async (courseId) => {
+        const teacherId = selectedTeacher[courseId];
+        if (!teacherId) {
+            alert("Please select a teacher.");
+            return;
+        }
+
+        try {
+            await axios.post('http://localhost:5002/api/update-course-teacher', {
+                course_id: courseId,
+                teacher_id: teacherId
+            }, {
+                headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+            });
+            alert("Teacher assigned successfully!");
+        } catch (error) {
+            console.error('Error assigning course teacher:', error);
+            alert("Failed to assign teacher. Please try again.");
+        }
+    };
 
     const handleDeleteCommitteeMember = async (exam_committee_id) => {
         try {
@@ -106,7 +188,7 @@ const ExamYearDetails = () => {
         }
     };
 
-    if (!examYearDetails) {
+    if (!examYearDetails || !teachers) {
         return <p>Loading...</p>;
     }
 
@@ -114,7 +196,6 @@ const ExamYearDetails = () => {
         <Container className="my-5">
             <h2 className="text-center mb-4">Exam Year Details</h2>
 
-            {/* Display Exam Year Details */}
             <Card className="mb-4 shadow-lg">
                 <Card.Header className="bg-primary text-white text-center">
                     <h4>Exam Year: {examYearDetails.Exam_year || 'N/A'}</h4>
@@ -128,7 +209,6 @@ const ExamYearDetails = () => {
                 </Card.Body>
             </Card>
 
-            {/* Display Class Representative Details */}
             {classRepresentative && studentProfile && (
                 <Card className="mb-4 shadow-lg">
                     <Card.Header className="bg-primary text-white text-center">
@@ -149,7 +229,6 @@ const ExamYearDetails = () => {
                 </Card>
             )}
 
-            {/* Display Exam Committee Members */}
             <Card className="mb-4 shadow-lg">
                 <Card.Header className="bg-primary text-white text-center">
                     <h4>Exam Committee Members</h4>
@@ -177,7 +256,6 @@ const ExamYearDetails = () => {
                 </Card.Body>
             </Card>
 
-            {/* Display List of Courses */}
             <Card className="mb-4 shadow-lg">
                 <Card.Header className="bg-primary text-white text-center">
                     <h4>Courses</h4>
@@ -185,22 +263,55 @@ const ExamYearDetails = () => {
                 <Card.Body>
                     <ListGroup className="mt-3">
                         {courses.length > 0 ? (
-                            courses.map((course) => (
-                                <ListGroup.Item key={course.course_id}>
-                                    <Row>
-                                        <Col>
-                                            <Link to={`/course-details/${course.course_id}`}>
-                                                {course.Course_code}
-                                            </Link>
-                                        </Col>
-                                        <Col>{course.Course_title}</Col>
-                                        <Col>{course.Course_type}</Col>
-                                        <Col>{course.Course_credit} Credits</Col>
-                                    </Row>
-                                </ListGroup.Item>
-                            ))
+                            courses.map((course) => {
+                                const teacher = teachers.find((t) => t.teacher_id === course.teacher_id);
+                                const teacherName = teacher ? teacher.Name : 'Not assigned';
+                                return (
+                                    <ListGroup.Item key={course.course_id}>
+                                        <Row>
+                                            <Col>{course.Course_title}</Col>
+                                            <Col>{course.Course_code}</Col>
+                                            <Col>{teacherName}</Col>
+                                            <Col>
+                                                <Form.Select
+                                                    aria-label="Select Department"
+                                                    value={selectedDepartment}
+                                                    onChange={handleDepartmentChange}
+                                                    className="mb-2"
+                                                >
+                                                    <option value="">Select Department</option>
+                                                    {uniqueDepartments.map((department, index) => (
+                                                        <option key={index} value={department}>
+                                                            {department}
+                                                        </option>
+                                                    ))}
+                                                </Form.Select>
+                                                {selectedDepartment && (
+                                                    <Form.Select
+                                                        aria-label="Select Teacher"
+                                                        onChange={(e) => handleTeacherChange(course.course_id, e.target.value)}
+                                                        value={selectedTeacher[course.course_id] || ""}
+                                                    >
+                                                        <option value="">Select Teacher</option>
+                                                        {filteredTeachers.map((teacher) => (
+                                                            <option key={teacher.teacher_id} value={teacher.teacher_id}>
+                                                                {teacher.Name}
+                                                            </option>
+                                                        ))}
+                                                    </Form.Select>
+                                                )}
+                                            </Col>
+                                            <Col>
+                                                <Button variant="primary" onClick={() => handleUpdateCourseTeacher(course.course_id)}>
+                                                    Assign
+                                                </Button>
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                );
+                            })
                         ) : (
-                            <p>No courses found for this exam year.</p>
+                            <p>No courses found.</p>
                         )}
                     </ListGroup>
                 </Card.Body>
